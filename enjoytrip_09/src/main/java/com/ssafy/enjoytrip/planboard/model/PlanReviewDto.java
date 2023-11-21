b6c5f010977b9865a95008edf0cec4d7bbb94561
yunjiL <luj2868@gmail.com>
2023-11-21 15:59:14 +0900

    Fix: review dto 관광지명 추가

--- enjoytrip_09/src/main/java/com/ssafy/enjoytrip/planboard/model/PlanReviewDto.java
+++ enjoytrip_09/src/main/java/com/ssafy/enjoytrip/planboard/model/PlanReviewDto.java
@@ -11,6 +11,7 @@ public class PlanReviewDto {
     private int articleNo;
     private int seq;
     private int contentId;
+    private String contentTitle;
     private String image;
     private String content;
 }
